bd84b6f6bb1db0071eb5ce094add1c79c96f8fbe
lashkevi4 <lashkevi4@users.noreply.github.com>
2023-06-05 17:17:48 +0300

    add new color in calrndar and del errors

--- html-js-css/my_calendar/js/script.js
+++ html-js-css/my_calendar/js/script.js
@@ -16,27 +16,42 @@ let week6 = document.getElementById('week6');
 function fillCalendar(daysInMonth, firstDayOfMonth) {
   let dayCounter = 1;
   let week = [week1, week2, week3, week4, week5, week6];
+  let today = new Date(); // текущая дата
 
   for (let i = 0; i < 6; i++) {
     for (let j = 0; j < 7; j++) {
       if ((i === 0 && j < firstDayOfMonth) || dayCounter > daysInMonth) {
         week[i].children[j].innerText = "";
+        week[i].children[j].classList.remove('current-day');  // Удаляем класс 'current-day', если он был присвоен
       } else {
         week[i].children[j].innerText = dayCounter;
+        // Если текущий месяц и год совпадают с отображаемыми в календаре, и текущий день совпадает с dayCounter, добавить класс "current-day"
+        if (currentDay.getFullYear() === today.getFullYear() && currentDay.getMonth() === today.getMonth() && today.getDate() === dayCounter) {
+          week[i].children[j].classList.add('current-day');
+        } else {
+          week[i].children[j].classList.remove('current-day');  // Удаляем класс 'current-day', если он был присвоен
+        }
         dayCounter++;
       }
     }
   }
 }
 
-fillCalendar(new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 0).getDate(), new Date(currentDay.getFullYear(), currentDay.getMonth(), 1).getDay());
+
+let firstDay = new Date(currentDay.getFullYear(), currentDay.getMonth(), 1).getDay();
+firstDay = firstDay === 0 ? 6 : firstDay - 1; // Воскресенье становится 6 (суббота), а остальные дни сдвигаются на 1
+fillCalendar(new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 0).getDate(), firstDay);
+
 
 // Отображение текущего месяца и года
 document.getElementById('currentMonthDisplay').textContent = `${currentMonthName} ${currentDay.getFullYear()}`;
 
 function changeMonth(increment) {
   currentDay.setMonth(currentDay.getMonth() + increment);
-  fillCalendar(new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 0).getDate(), new Date(currentDay.getFullYear(), currentDay.getMonth(), 1).getDay());
+  let firstDay = new Date(currentDay.getFullYear(), currentDay.getMonth(), 1).getDay();
+  firstDay = firstDay === 0 ? 6 : firstDay - 1; // Воскресенье становится 6 (суббота), а остальные дни сдвигаются на 1
+  fillCalendar(new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 0).getDate(), firstDay);
+
   currentMonthName = monthNames[currentDay.getMonth()];
   document.getElementById('currentMonthDisplay').textContent = `${currentMonthName} ${currentDay.getFullYear()}`;
 }
