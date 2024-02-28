4692e318896123c4e66824b5261527a5cc5fbd9c
lashkevi4 <lashkevi4@users.noreply.github.com>
2024-02-28 12:15:43 +0200

    update

--- React/SmartClock/sleep-cycle-app/src/App.js
+++ React/SmartClock/sleep-cycle-app/src/App.js
@@ -10,28 +10,33 @@ function App() {
     sleepTime.setHours(time.substring(0, 2), time.substring(3), 0, 0);
 
     const recommendedWakeUpTimes = [];
+    const sleepDurations = [];
     for (let cycle = 4; cycle <= 6; cycle++) {
       const wakeTime = new Date(sleepTime.getTime() + cycle * 90 * 60 * 1000);
-      recommendedWakeUpTimes.push(wakeTime.toLocaleTimeString().substring(0, 5));
+      recommendedWakeUpTimes.push({
+        time: wakeTime.toLocaleTimeString().substring(0, 5),
+        duration: cycle * 1.5 // Продолжительность сна в часах
+      });
     }
 
     setWakeUpTimes(recommendedWakeUpTimes);
   };
 
+
   return (
     <div className="App">
-      <h1>Выберите время засыпания</h1>
+      <h1>Время засыпания</h1>
       <SleepTimeInput onTimeSelect={handleTimeSelect} />
-      {wakeUpTimes.length > 0 && (
-        <div className="recommended-times">
-          <h2>Время пробуждения:</h2>
-          {wakeUpTimes.map((time, index) => (
-            <div key={index}>{time}</div>
-          ))}
-        </div>
-
-      )}
+      <div className="recommended-times">
+        <h2>Время пробуждения</h2>
+        {wakeUpTimes.map((item, index) => (
+          <div key={index}>
+            {item.time} - {item.duration.toFixed(1)} часов сна
+          </div>
+        ))}
+      </div>
     </div>
+
   );
 }
 
